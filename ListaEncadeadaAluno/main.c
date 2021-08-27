e070e161c59c03d84209ef4ef1d16c0e854668f7
Mauro Lopes <maurosilva@ifma.edu.br>
2021-08-27 09:57:11 -0300

    Atualização com alocação dinamica

--- ListaEncadeadaAluno/main.c
+++ ListaEncadeadaAluno/main.c
@@ -19,11 +19,15 @@ int main()
     Aluno meualuno;
 
 
+
+
+
     printf("Informe a matricula do Aluno:");
     scanf("%d",&meualuno.matricula);
     fflush(stdin);
     printf("Informe o nome do Aluno:");
     fgets(meualuno.nome,60,stdin);
     printf("Matricula:%d\nNome:%s",meualuno.matricula,meualuno.nome);
+
     return 0;
 }
